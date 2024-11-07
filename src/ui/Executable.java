01dc930b13710fdc0804c4277f2544ad016e7c6f
MIKE <159577452+HydraMi@users.noreply.github.com>
2024-11-07 17:55:41 -0500

    P1

--- src/ui/Executable.java
+++ src/ui/Executable.java
@@ -1,6 +1,6 @@
 package ui;
 
-import java.util.*;
+import java.util.Scanner;
 import model.Controller;
 
 public class Executable {
@@ -38,8 +38,8 @@ public class Executable {
 		while (!flag) {
 
 			System.out.println("\n \n Bienvenido al menu:\n");
-			System.out.println("Opciones:\n" + "1. Fixture \n" 
-					+ "2. Precargar informacion \n" + "3. Realizar partido entre 2 equipos \n" + "4. Salir del programa \n");
+			System.out.println("Opciones:\n" + "1. Precargar informacion \n" 
+					+ "2. Fixture \n" + "3. Realizar partido entre 2 equipos \n" + "4. Salir del programa \n");
 
 			int option = escaner.nextInt();
 
@@ -47,15 +47,16 @@ public class Executable {
 
 			switch (option) {
 					case 1:
-
+                        preload();
 						break;
 					case 2:
-
+                        fixture();
 						break;
 					case 3:
-
+                        partido();
 						break;
 					case 4:
+                        System.out.println("Saliendo de la app...");
 						flag = true;
 						System.exit(0);
 						break;
@@ -69,18 +70,31 @@ public class Executable {
     }
 
     /**
-     * Método principal (main) para iniciar la ejecución del programa.
-     *
      * @pre No se requieren precondiciones específicas.
      * @post Se crea una instancia de Executable y se ejecuta el programa principal.
-     * @param args Los argumentos de la línea de comandos (no se utilizan en este
-     *             caso).
+     * @param args Los argumentos de la línea de comandos.
      */
     public static void main(String[] args) {
 
         Executable mainApp = new Executable();
         mainApp.run(flag);
 
+    }
+
+    public void preload() {
+        controladora.preload();
+        System.out.println("Informacion precargada exitosamente.");
+
+        System.out.println(controladora.mostrarEquipos());
+        System.out.println(controladora.mostrarArbitros());
+    }
+
+    public void fixture() {
+        System.out.println(controladora.fixture());
+    }
+
+    public void partido() {
+        System.out.println(controladora.jugada());
     }
 
 }
